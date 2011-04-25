7df8aafc177e7b0ae096203739aa7d7a81de5fd3
Shawn M Moore <sartak@bestpractical.com>
2011-04-25 12:12:57 -0400

    Remove call to Test::Builder->exported_to
        From the doc:
            This method isn't terribly useful since modules which share the
            same Test::Builder object might get exported to different packages
            and only the last one will be honored.
    
        I can reinstate it if needed but I hope no one's depending upon it!

--- lib/Test/Spelling.pm
+++ lib/Test/Spelling.pm
@@ -26,7 +26,6 @@ sub import {
     *{$caller.'::all_pod_files'}             = \&all_pod_files
         unless defined &{$caller. '::all_pod_files'};
 
-    $Test->exported_to($caller);
     $Test->plan(@_);
 }
 
